4bd564e9a127616b425b705e7daf586840b43eb3
Aaron <Aaron@192.168.0.100>
2014-06-29 07:06:04 -0400

    Test Checkin

---
+++ Gorm-Example/src/java/com/amondel2/TechTalk/Constants.java
@@ -0,0 +1,7 @@
+package com.amondel2.TechTalk;
+
+import java.util.UUID;
+
+public class Constants {
+	 static final UUID baseUUID = java.util.UUID.fromString("00000000-0000-0000-0000-000000000000");
+}

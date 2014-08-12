c401309a074959bf2602ca8cc63f21011383abde
Mondelblatt, Aaron <amondelb@us.ibm.com>
2014-08-12 00:19:02 -0400

    Geb Spec against my web app

--- GebExample/test/functional/WebAppCRUDSpec.groovy
+++ GebExample/test/functional/WebAppCRUDSpec.groovy
@@ -7,19 +7,49 @@ class WebAppCRUDSpec  extends GebReportingSpec {
     def "Go to WebApp"() {
 	when:
 	to WebAppPage
-    	then:
-	  1 == 1
-	
-    }
-    
-    def "Get Title"() {
-	when:
-	def cname = getCompanyDB().name
-	wiatFor{
-	    company.text == name
-	}
 	then:
 	1 == 1
+
     }
 
+    def "Get Orgs"() {
+	when:
+	openCompany()
+	then:
+	organizationItems.size() > 1
+    }
+
+    def "Get Jobs"() {
+	when:
+	openJobs()
+	then:
+	jobItemsFirst.size() > 1
+    }
+
+    def "open Employees"() {
+	when:
+	openEmployees()
+	then:
+	employeeItems.size() > 1
+    }
+
+    def "remane Company Test"() {
+	given:
+	def newName = "A better named company"
+	when:
+	rename(newName)
+	then:
+	company.text().trim().startsWith(newName) == true
+	pageHeading.text() == "Welcome to " + newName
+
+    }
+
+    def "put orginal name back"() {
+
+	when:
+	println "cleaning up"
+	origName()
+	then:
+	pageHeading.text() == "Welcome to " + getOrginalCompanyName()
+    }
 }
--- GebExample/test/functional/pages/WebAppPage.groovy
+++ GebExample/test/functional/pages/WebAppPage.groovy
@@ -3,27 +3,69 @@ package pages
 import geb.Page;
 
 import com.amondel2.techtalk.Company;
+import com.amondel2.techtalk.Organization
 
 class WebAppPage extends Page {
+
+    def orginalCompanyName
+    
     static url = "http://localhost:8092/WebExample/"
     static at = {
 	title ==~ /WebApp Example/
     }
 
     static content = {
-	company{ $(this.getCompanyDB().id) }
-	searchButton(wait: true){$("#gbqfb")}
-	searchResult{ $("div.rc")}
-    }
-    
-    void getCompanyDB() {
-	Company.first()
-    }
-    
-   
-   
+	company(wait: true){ $("div.jstree").find("ul").first().find("li").first() }
+	companyButton(wait: true){ $(company).find("i.jstree-ocl") }
+	companyRightClick(wait:true) { $(company).find("div").first() }
+	organizationItems(wait: true){  $(company).find("ul.jstree-children li")}
+	organizationButtons(wait: true){  $(organizationItems).find("i.jstree-ocl")}
+	jobItemsFirst(wait: true){ $(organizationItems).find("ul.jstree-children li")}
+	jobItemsButtons(wait: true){ $(jobItemsFirst).find("i.jstree-ocl")}
+	employeeItems(wait:true){$(jobItemsFirst).find("ul.jstree-children li")}
+	jsmenu{$("ul.jstree-contextmenu")}
+	jsmenuRename{$(jsmenu).find( "li")[1].find('a')}
+	pageHeading{$("h1.ng-binding")}
 
-    void rename(String selector,String searchTerm) {
-		
+    }
+
+    void openJobs() {
+	organizationButtons[0].click()
+	waitFor{
+	    jobItemsFirst.size() > 1
+	}
+
+    }
+
+    void openEmployees() {
+	jobItemsButtons[0].click()
+	waitFor{
+	    employeeItems.size() > 1
+	}
+    }
+
+    void openCompany() {
+	this.orginalCompanyName = company.text().trim()
+	println "company name " + this.orginalCompanyName
+	companyButton.click()
+	waitFor{
+	    organizationItems.size() > 1
+	}
+
+    }
+    
+    void origName() {
+	this.rename(this.orginalCompanyName)
+    }
+
+    void rename(String newName) {
+	companyRightClick.jquery.contextmenu()
+	waitFor{
+	    jsmenu.jquery.css('display') == 'block'
+	}
+	jsmenuRename.click()
+	$("span.jstree-clicked input").value newName
+    	//need a blur
+	$("span.jstree-clicked input").jquery.blur()
     }
 }
